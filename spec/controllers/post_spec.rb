a81955430d7c69c9c65b3d81531d0fe3e53762b8
Kevin <k3vinwei@gmail.com>
2020-10-19 19:41:13 +0800

    exit louisa

--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,10 +1,38 @@
 require "rails_helper"
 
 RSpec.describe PostsController, type: :controller do
+    before(:all) do
+        @post1 = Post.create(title: "title1", content: "content1")
+        @post2 = Post.create(title: "title2", content: "content2")
+    end
+
     it "#index" do
         get :index 
         expect(response.status).to be 200
         expect(response).to render_template :index
+    end
+
+    it '#new' do
+        get :new
+        expect(response.status).to be 200
+        expect(response).to render_template :new
+    end
+
+    it '#edit' do
+        get :edit, params: { id: @post1[:id]}
+        expect(response.status).to be 200
+        expect(response).to render_template :edit
+    end
+
+    describe "#create" do
+        before(:all) do
+            @post = { title: "title", content: "content" }
+        end
+
+        it " create record " do
+            expect( post :create, params: @post ).to change( Post.all.size )
+                .by(1)
+        end
 
     end
 end
