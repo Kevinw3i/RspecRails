8a016057c3712e58e90834349f0671ae6ccb7861
Kevin <k3vinwei@gmail.com>
2020-10-19 16:34:54 +0800

    def remove not use file

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module RspecDemo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.controller_specs false
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
   end
 end
