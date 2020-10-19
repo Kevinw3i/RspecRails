2a8994b7958137012d8c0df56c97b8b2999f586b
Kevin <k3vinwei@gmail.com>
2020-10-19 17:58:29 +0800

    Post model Rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  # 測試是否能 create Post 
+  it "accessible" do
+    post = Post.create!
+    expect(post).to eq(Post.last)
+  end
 
+  # 
+  it "has title and content columns" do
+    columns = Post.column_names
+    # column_name 取得 ['id', 'title', 'content']
+    expect(columns).to include('id')
+    expect(columns).not_to include('idst')
+    expect(columns).to include('title')
+    expect(columns).to include('content')
+  end
 end
